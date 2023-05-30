081403245e2ba3cecbf790146e0e76a7d324cdae
ivabox <123444353+ivabox@users.noreply.github.com>
2023-05-30 15:48:58 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,39 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-panorama--1-copy",
-      "name": "PANORAMA  (1) copy",
+      "id": "0-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1",
+      "name": "1",
       "levels": [
         {
           "tileSize": 256,
